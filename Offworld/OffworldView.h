8eb56fb9d9baf3c0ccf6051ace4140aa72fde6a7
Mike Killewald <mkillewald@yahoo.com>
2017-04-11 15:43:44 -0400

    Initial Commit

---
+++ Offworld/OffworldView.h
@@ -0,0 +1,13 @@
+//
+//  OffworldView.h
+//  Offworld
+//
+//  Created by k1ds3ns4t10n on 4/11/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface OffworldView : ScreenSaverView
+
+@end
---
+++ Offworld/OffworldView.m
@@ -0,0 +1,52 @@
+//
+//  OffworldView.m
+//  Offworld
+//
+//  Created by k1ds3ns4t10n on 4/11/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+#import "OffworldView.h"
+
+@implementation OffworldView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
